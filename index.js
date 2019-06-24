32ff21cca0ac3b4036b3ba98ca5728b63101ffe5
Anna Fulton <afulton7@gmail.com>
2019-06-24 14:10:25 -0400

    GET /cats with optional breed filter query string

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ app.get('/', (req, res) => {
   res.status(200).send('Welcome to all the cats. Meow.')
 })
 
+// get cats & allow for query on breeds
+app.get('/cats', (req, res) => {
+  res
+    .status(200)
+    .send(
+      catsDatabase.filter(
+        req.query.breed
+          ? item => item.type === 'cat' && item.breed === req.query.breed
+          : item => item.type === 'cat'
+      )
+    )
+})
+
+// get breeds
+app.get('/breeds', (req, res) => {
+  res.status(200).send(catsDatabase.filter(item => item.type === 'breed'))
+})
+
 app.listen(process.env.PORT || 5555, process.env.HOST || '127.0.0.1', () => {
   console.log("I'm up and listening on http://127.0.0.1:5555")
 })
